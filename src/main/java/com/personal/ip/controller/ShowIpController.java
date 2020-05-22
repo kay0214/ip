6674e913988a1540e377ba39f14543a2b7446134
sunpeikai <spk123456>
2020-05-22 15:27:33 +0800

    Initial commit

---
+++ src/main/java/com/personal/ip/controller/ShowIpController.java
@@ -0,0 +1,52 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.personal.ip.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author sunpeikai
+ * @version ShowIpController, v0.1 2020/5/6 15:13
+ * @description
+ */
+@RestController
+@RequestMapping("/ip")
+public class ShowIpController {
+
+    @GetMapping("/show")
+    public String show(HttpServletRequest request){
+        String ip = getIp(request);
+        System.out.println("ip:[" + ip + "]");
+        return "thanks";
+    }
+
+    private static String getIp(HttpServletRequest request) {
+        if (request == null) {
+            return "unknown";
+        }
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
